3dce7b9f734183b1ec1843f3f0e7346fd31cbc72
Artsiom <sorintema@gmail.com>
2020-03-31 22:35:29 +0300

    aditing of todos and lots more

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('creating_date', )
+
+admin.site.register(Todo, TodoAdmin)
 # Register your models here.
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField(blank=True)
+    creating_date = models.DateTimeField(auto_now_add=True)
+    completing_date=models.DateTimeField(null=True, blank=True)
+    importance=models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
+    def __str__(self):
+        return self.title
 # Create your models here.
