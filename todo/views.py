082573dd9155fa6f46fe7c86b185fd3730167be5
Artsiom <sorintema@gmail.com>
2020-04-01 23:22:53 +0300

    all login system is made, disigned is left

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,8 @@ from django.db import IntegrityError
 from django.contrib.auth import login,logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -25,6 +27,7 @@ def signupuser(request):
             print("error")
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error':'passwod didn\'t math'})
 
+
 def loginuser(request):
     if request.method=='GET':
         return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
@@ -36,11 +39,13 @@ def loginuser(request):
             login(request, user)
             return redirect('currenttodos')
 
+@login_required
 def logoutuser(request):
     if(request.method=="POST"):
         logout(request)
         return redirect(home)
 
+@login_required
 def createtodo(request):
     todos=Todo.objects.filter(user=request.user, completing_date__isnull=True)
     if request.method == 'GET':
@@ -55,15 +60,17 @@ def createtodo(request):
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(),'error' : 'value in form doesn\'t mathc'})
 
+@login_required
 def currenttodos(request):
     todos= Todo.objects.filter(user=request.user, completing_date__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos':todos})
 
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'GET':
         form = TodoForm(instance=todo)
-        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+        return render(request, 'todo/viewtodo.html', {'todo' : todo, 'form':form})
     else:
         try:
             form = TodoForm(request.POST, instance=todo)
@@ -72,4 +79,22 @@ def viewtodo(request, todo_pk):
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form,'error' : 'value in form doesn\'t mathc'})
 
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.completing_date = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
 
+@login_required
+def deletetodo(request, todo_pk ):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, completing_date__isnull=False).order_by('-completing_date')
+    return render(request, 'todo/copletedtodos.html', {'todos': todos})
